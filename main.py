e6fb418646dd43a0b42137c21ce14f69389ff3e0
Naotada Nakajima <naotada@NaotadanoMacBook-ea.local>
2019-12-25 18:23:09 +0900

    new commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+<<<<<<< HEAD
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (
@@ -8,20 +9,45 @@ import os
 import get_keyword_photo as gtk
 
 app = Flask(__name__)
+=======
+ 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
+ 
+app = Flask(__name__)
+ 
+>>>>>>> heroku/master
 #環境変数取得
 # LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+<<<<<<< HEAD
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+=======
+ 
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+ 
+ 
+>>>>>>> heroku/master
 ## 1 ##
 #Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
     # リクエストヘッダーから署名検証のための値を取得します。
     signature = request.headers['X-Line-Signature']
+<<<<<<< HEAD
 
     # リクエストボディを取得します。
     body = request.get_data(as_text=True)
@@ -37,10 +63,28 @@ def callback():
     #handleの処理を終えればOK
     return 'OK'
 
+=======
+ 
+    # リクエストボディを取得します。
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+ 
+    # handle webhook body
+    #  署名を検証し、問題なければhandleに定義されている関数を呼び出す。
+    try:
+        handler.handle(body, signature)
+    # 署名検証で失敗した場合、例外を出す。
+    except InvalidSignatureError:
+        abort(400)
+    # handleの処理を終えればOK
+    return 'OK'
+ 
+>>>>>>> heroku/master
 ## 2 ##
 ###############################################
 #LINEのメッセージの取得と返信内容の設定(オウム返し)
 ###############################################
+<<<<<<< HEAD
 
 #LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
 #def以下の関数を実行します。
@@ -58,9 +102,27 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(body=show_img_url)) #ここでオウム返しのメッセージを返します。
     
+=======
+ 
+#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
+#def以下の関数を実行します。
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+ 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
+ 
+>>>>>>> heroku/master
 # ポート番号の設定
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
+<<<<<<< HEAD
     app.run(host="0.0.0.0", port=port)
 #デプロイ
+=======
+    app.run(host="0.0.0.0", port=port)
+>>>>>>> heroku/master

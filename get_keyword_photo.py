e02044744d49bdabbcf4e48da85834a46a6a5418
Naotada Nakajima <naotada@NaotadaacBookea.toshima.ne.jp>
2019-12-31 15:21:21 +0900

    new commit

--- get_keyword_photo.py
+++ get_keyword_photo.py
@@ -10,11 +10,47 @@ from io import BytesIO
 from PIL import Image, ImageOps
 import requests
 
+CLIENT_INFO ={
+    "installed":{
+        "client_id":"902572033754-mk5cvvgjs8ld0t4bbhuho072aolvrsik.apps.googleusercontent.com",
+        "project_id":"modern-triumph-254209",
+        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+        "token_uri":"https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret":"SPErn7SRfLjoGmpUiSjOfqb9",
+        "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
+        }
+    }
+
+CREDENTIAL_INFO ={
+    "access_token": "ya29.ImC2B0YLJJrH4Y8Lrr4Dxkad5b8_Uwx9wxbsgaoRN5CXZhvhbzk_ZyqiAYSXXrT69oLrkdTGqsi0CWJsRQN0DEz718cRt4ccE7hCS_XEfe_UKq1eiNdNBTv7fUt-nCeTSZY", 
+    "client_id": "902572033754-mk5cvvgjs8ld0t4bbhuho072aolvrsik.apps.googleusercontent.com", 
+    "client_secret": "SPErn7SRfLjoGmpUiSjOfqb9", 
+    "refresh_token": "1//0e4x02R0e0QBpCgYIARAAGA4SNwF-L9Ir6p0a8H4v_FWz4aeA4e40dlGk9LBHHZG9VrIKPHM2wn_UIZWn9mzRd1N86zcDV_6xsaQ", 
+    "token_expiry": "2019-12-21T06:59:49Z", 
+    "token_uri": "https://oauth2.googleapis.com/token", 
+    "user_agent": "photolibrary", 
+    "revoke_uri": "https://oauth2.googleapis.com/revoke", 
+    "id_token": None, 
+    "id_token_jwt": None, 
+    "token_response": {
+        "access_token": "ya29.ImC2B0YLJJrH4Y8Lrr4Dxkad5b8_Uwx9wxbsgaoRN5CXZhvhbzk_ZyqiAYSXXrT69oLrkdTGqsi0CWJsRQN0DEz718cRt4ccE7hCS_XEfe_UKq1eiNdNBTv7fUt-nCeTSZY", 
+        "expires_in": 3600, 
+        "scope": "https://www.googleapis.com/auth/photoslibrary", 
+        "token_type": "Bearer"}, 
+    "scopes": ["https://www.googleapis.com/auth/photoslibrary"], 
+    "token_info_uri": "https://oauth2.googleapis.com/tokeninfo", 
+    "invalid": False, 
+    "_class": "OAuth2Credentials", 
+    "_module": "oauth2client.client"
+    }
+
+
 SCOPES = "https://www.googleapis.com/auth/photoslibrary"
 
-CLIENT_SECRET_FILE_PATH = "client_id(3)_Pht.json"
+CLIENT_SECRET_FILE_PATH = CLIENT_INFO
 
-USER_SECRET_FILE_PATH = 'credentials-photoslibrary.json'
+USER_SECRET_FILE_PATH = CREDENTIAL_INFO
 
 flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 

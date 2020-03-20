6d5f15184a0c1c82af712d86717d2d5d2a67f79b
Naotada Nakajima <naotada@NaotadanoMacBook-ea.local>
2020-03-20 16:12:19 +0900

    修正（後でもう一回する）

--- get_web_schedules.py
+++ get_web_schedules.py
@@ -27,16 +27,17 @@ def get_a_schedule():
 
     for event in get_schedule:
         for a in event.select("a"):
+            event_set = []
             href = a.attrs["href"]
-            event_set = a.getText(),href
+            event_set += a.getText(),href
             event_list.append(event_set)
 #    event_list = dict(event_list)
     return event_list
     
-def choice_a_day(keyword,event_info):
+def choice_a_day(keyword,events_list):
     keyword = keyword.translate(henkan_table)
     today = datetime.date.today()
-    event_info = dict(event_info)
+    event_info = dict(events_list)
     selected_day = ()
     key = "エラー発生"
     if "月" in keyword and "日" in keyword:
@@ -58,21 +59,63 @@ def choice_a_day(keyword,event_info):
             selected_day = str(today.year) + keyword
         except KeyError as key:
             return key
-            
+
+    a_day_info = []
+    count = 0
     for a_day in event_info.keys():
         if selected_day in a_day:
-            a_day_info = ()
+            a_info = ()
             edit_one = event_info[a_day].replace("\n", "")
             a_day = a_day.replace("\n", "")
-            a_day_info = a_day + "\n" + edit_one
-            return a_day_info
-            break
+            a_info = a_day + "\n" + edit_one
+            a_day_info.append(a_info)
+            count += 1
+            if count == len(events_list.keys()):
+                break
+            else:
+                continue
+        elif selected_day not in a_day:
+            count += 1
+            if count == len(events_list.keys()):
+                break
+            else:
+                continue
         else:
-            continue
+            return Exception
+    if len(a_day_info) == 1:
+        return a_day_info
+    else:
+        a_day_info = "\n\n".join(a_day_info)
+        return a_day_info
 
 def get_a_earlist_schedule(events_list):
-    a_earlist_one = events_list[0][0].replace("\n", "") + "\n" +events_list[0][1]
-    return a_earlist_one
+    today = datetime.date.today()
+    today = str(today.year) + "/" + str(today.month) + "/" + str(today.day)
+    count = 0
+    a_day_info = []
+    for num in range(len(events_list)):
+        if today in events_list[num][0][1]:
+            count += 1
+            if count >= len(events_list):
+                break
+            else:
+                continue
+        else:
+            for a_day in events_list:
+                if a_day[0][1] == events_list[num][0][1]:
+                    a_day_info.append(events_list[num])
+                    count += 1
+                    if count == len(events_list):
+                        break
+                    else:
+                        continue
+    if len(a_day_info) == 1:
+        return a_day_info
+    else:
+        a_day_info = "\n\n".join(a_day_info)
+        return a_day_info
+#    a_earlist_one = events_list[0][0].replace("\n", "") + "\n" +events_list[0][1]
+#    return a_earlist_one
 
 def get_today_event(events_list):
     today = datetime.date.today()

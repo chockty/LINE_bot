44183b18c13f68ede27cb65cd7364fad07bdbe59
Naotada Nakajima <naotada@NaotadanoMacBook-ea.local>
2020-03-21 17:16:49 +0900

    一番最近のスケジュール表示のためのコードを最実装

--- get_web_schedules.py
+++ get_web_schedules.py
@@ -37,7 +37,7 @@ def get_a_schedule():
 def choice_a_day(keyword,events_list):
     keyword = keyword.translate(henkan_table)
     today = datetime.date.today()
-    event_info = dict(events_list)
+    events_info = dict(events_list)
     selected_day = ()
     key = "エラー発生"
     if "月" in keyword and "日" in keyword:
@@ -62,10 +62,10 @@ def choice_a_day(keyword,events_list):
 
     a_day_info = []
     count = 0
-    for a_day in event_info.keys():
+    for a_day in events_info.keys():
         if selected_day in a_day:
             a_info = ()
-            edit_one = event_info[a_day].replace("\n", "")
+            edit_one = events_info[a_day].replace("\n", "")
             a_day = a_day.replace("\n", "")
             a_info = a_day + "\n" + edit_one
             a_day_info.append(a_info)
@@ -89,31 +89,77 @@ def choice_a_day(keyword,events_list):
         return a_day_info
 
 def get_a_earlist_schedule(events_list):
-    today = datetime.date.today()
-    today = str(today.year) + "/" + str(today.month) + "/" + str(today.day)
-    count = 0
+    hantei_list = []
+    for y_m_d in event_lists:
+        test = test[0].split("\n")
+        hantei_list.append(test[1])
+    
     a_day_info = []
-    for num in range(len(events_list)):
-        if today in events_list[num][0][1]:
+    event_list = dict(event_list)
+    count = 0    
+    for test2 in hantei_list:
+        if test2 == hantei_list[0]:
             count += 1
-            if count >= len(events_list):
-                break
-            else:
+    
+    for a_day in event_list.keys():
+        if hantei_list[0] in a_day:
+            a_info = ()
+            edit_one = event_list[a_day].replace("\n", "")
+            a_day = a_day.replace("\n", "")
+            a_info = a_day + "\n" + edit_one
+            a_day_info.append(a_info)
+            count -= 1
+            if count > 0:
                 continue
+            elif count == 0:
+                break
         else:
-            for a_day in events_list:
-                if a_day[0][1] == events_list[num][0][1]:
-                    a_day_info.append(events_list[num])
-                    count += 1
-                    if count == len(events_list):
-                        break
-                    else:
-                        continue
+            return Exception
+    
+    elif hantei_list[0] not in a_day:
+        count -= 1
+        if count > 0:
+            continue
+        elif count == 0:
+            break
+        else:
+            return Exception
+    else:
+        return Exception
+    
     if len(a_day_info) == 1:
-        return a_day_info
+        a_day_info = "\n".join(a_day_info)
+        print(a_day_info)
     else:
         a_day_info = "\n\n".join(a_day_info)
-        return a_day_info
+        print(a_day_info)
+
+
+#    today = datetime.date.today()
+#    today = str(today.year) + "/" + str(today.month) + "/" + str(today.day)
+#    count = 0
+#    a_day_info = []
+#    for num in range(len(events_list)):
+#        if today in events_list[num][0][1]:
+#            count += 1
+#            if count >= len(events_list):
+#                break
+#            else:
+#                continue
+#        else:
+#            for a_day in events_list:
+#                if a_day[0][1] == events_list[num][0][1]:
+#                    a_day_info.append(events_list[num])
+#                    count += 1
+#                    if count == len(events_list):
+#                        break
+#                    else:
+#                        continue
+#    if len(a_day_info) == 1:
+#        return a_day_info
+#    else:
+#        a_day_info = "\n\n".join(a_day_info)
+#        return a_day_info
 #    a_earlist_one = events_list[0][0].replace("\n", "") + "\n" +events_list[0][1]
 #    return a_earlist_one
 
@@ -150,7 +196,4 @@ def get_today_event(events_list):
     else:
         a_day_info = "\n\n".join(a_day_info)
         return a_day_info
-
-
-
 
